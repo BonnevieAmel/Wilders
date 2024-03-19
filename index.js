82629cab9588b800bf40ff0d2218222784f6eb85
BonnevieAmel <amel.bonnevie@gmail.com>
2024-03-19 11:30:50 +0100

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+const name1 = 'Lauryn';
+const name2 = 'Maeva';
+const name3 = 'Rachel';
+const name4 = 'Angelina';
+
+import chalk from 'chalk';
+
+console.log(chalk.blue(name1));
+console.log(chalk.magenta(name2));
+console.log(chalk.green(name3));
+console.log(chalk.yellow(name4));
+
+
---
+++ information.js
@@ -0,0 +1,10 @@
+
+  
+  module.exports = {
+  
+    name1: name1,
+    name2: name2,
+    name3: name3,
+    name4: name4,
+  
+  };
